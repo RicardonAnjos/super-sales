76688047fefa4217bec76008acdf0651eb6d89e5
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-03-02 16:02:28 -0300

    iniciando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
